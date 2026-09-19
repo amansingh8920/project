3f784eede4e94b11b2d14ad2304775b3b50f97db
amansingh8920 <deepsingh8920@gmail.com>
2026-09-19 21:35:21 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './App.css';
-import Project from './Project'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import Nav from './Nav.jsx'
-import Takht from './Takht.jsx';
+
 import Footer from './Footer.jsx'
-import Hukamnamapage from './Hukamnamapage.jsx';
-import Sarovar from './Sarovar.jsx'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-      <BrowserRouter>
-       <Routes>
-         <Route path='/' element={<Project/> }></Route>
-          <Route path='/takht' element={<Takht/> }></Route>
-          <Route path='/hukamnama' element={<Hukamnamapage/> }></Route>
-          <Route path='/sarovar' element={<Sarovar/> }></Route>
-       </Routes>
-      </BrowserRouter>
+      
       <Footer />
     </div>
   );
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,52 +1,54 @@
 import sgpcImage from './image/sgpcbanner.png'
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Project from './Project'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Hukamnamapage from './Hukamnamapage.jsx';
+import Sarovar from './Sarovar.jsx'
+import Takht from './Takht.jsx';
 
-export default function Nav(){
+export default function Nav() {
 
-      const [hide, setHide] = useState(true);
-    
-        const handle = () => {
-            if (hide) {
-                document.getElementById('lis').style.display = "block";
-                setHide(false);
-            } else {
-                setHide(true);
-                document.getElementById('lis').style.display = "none";
-            }
-        }
+    const [hide, setHide] = useState(true);
 
-        const handle1=()=>{
-            document.location='/takht'
+    const handle = () => {
+        if (hide) {
+            document.getElementById('lis').style.display = "block";
+            setHide(false);
+        } else {
+            setHide(true);
+            document.getElementById('lis').style.display = "none";
         }
-        
-        const handle2=()=>{
-            document.location='/sarovar'
-        }
+    }
 
-        const handle3=()=>{
-            document.location='/hukamnama'
-        }
 
-        const handle4=()=>{
-            document.location='/'
-        }
-    
+
     return <div>
-        
-           <nav>
-                    <div className="nav-item"><img src={sgpcImage} alt="sgpc logo" id="sgpc" onClick={handle4}/></div>
-                    <div className="nav-item">SGPC ਸ੍ਰੀ ਅਮ੍ਰਿਤਸਰ </div>
-                    <div className="nav-item" id="menu" onClick={handle}>Menu</div>
-            </nav>
-        
-        
-                <div >
-                    <ul id="lis">
-                       <li className='lis-item' onClick={handle1}>5 Takht</li>
-                        <li className='lis-item' onClick={handle2}>5 Sarovar</li>
-                        <li className="lis-item" onClick={handle3}>Hukamnama</li>
-                       
-                    </ul>
-                </div>
+
+        <nav>
+            <div className="nav-item"><img src={sgpcImage} alt="sgpc logo" id="sgpc"/></div>
+            <div className="nav-item">SGPC ਸ੍ਰੀ ਅਮ੍ਰਿਤਸਰ </div>
+            <div className="nav-item" id="menu" onClick={handle}>Menu</div>
+        </nav>
+
+
+        <div >
+
+            <HashRouter>
+                <ul id="lis">
+                    <li className="lis-item" ><Link to="/">Home</Link></li>
+                    <li className='lis-item' ><Link to="/takht">Takht</Link></li>
+                    <li className='lis-item' ><Link to="/sarovar">Sarovar</Link></li>
+                    <li className="lis-item" ><Link to="/hukamnama">Hukamnama</Link></li>
+
+                </ul>
+                <Routes>
+                    <Route path='/' element={<Project />}></Route>
+                    <Route path='/takht' element={<Takht />}></Route>
+                    <Route path='/hukamnama' element={<Hukamnamapage />}></Route>
+                    <Route path='/sarovar' element={<Sarovar />}></Route>
+                </Routes>
+            </HashRouter>
+        </div>
     </div>
 }
